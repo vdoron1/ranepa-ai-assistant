d997683030f2309ccd7bec4921bc9835380f25e0
vdoron1 <vdoron05@gmail.com>
2025-01-31 22:58:19 +0300

    Initial commit

--- vkbot/main.py
+++ vkbot/main.py
@@ -8,7 +8,7 @@ from config import AppConfig
 from vkbottle import Bot
 from src.context.middlewares import RedisMiddleware
 
-bot = Bot("vk1.a.ryCeD1x_Oi-eT4r3A_sncfCzzP3BF4S-cQrHWNRg9NwfldJTF9x_4qBoPO9nAHxBbkRhR7TdKlxXa2Q4imptjdl2uQEGk3sDGgQBOOAfhK2x8ZmQVu_9M8s-CHrkc4Pyd4Z1bLWIFVHoqVz7KrWcoiBEqKunzxXvzLN-tieOAUUJz6etJS6tZJ0ZGpi0EVSWer0KyCtSZUDiCpqMi2OXng")
+bot = Bot('VK_TOKEN')
 
 # Регистрируем Middleware
 bot.labeler.message_view.register_middleware(RedisMiddleware)
