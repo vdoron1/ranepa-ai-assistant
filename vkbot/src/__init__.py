bbf38045a8332373a4b4261f56f8d9cf011483fb
vdoron1 <vdoron05@gmail.com>
2025-02-01 15:25:26 +0300

    Source Control

--- vkbot/src/__init__.py
+++ vkbot/src/__init__.py
@@ -1,10 +1,20 @@
 from vkbottle.bot import BotLabeler
 from .controllers import chat_router, clear_router
+from .controllers.middlewares import NoBotMiddleware, RegistrationMiddleware, InfoMiddleware
 
 
 router = BotLabeler()
+
+
+router.message_view.register_middleware(NoBotMiddleware())
+router.message_view.register_middleware(RegistrationMiddleware())
+router.message_view.register_middleware(InfoMiddleware())
+
 
 router.load(clear_router)
 router.load(chat_router)
 
 __all__ = ("router",)
+
+
+
